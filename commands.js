09b982b2e087181f0573c0f4b3ee33ff2a88f1e6
shikto-48 <90758703+shikto-48@users.noreply.github.com>
2021-09-27 17:20:53 +0600

    Add files via upload

--- commands.js
+++ commands.js
@@ -12,5 +12,13 @@ export let COMMANDS = {
             console.log("https://crafatar.com/renders/head/" + data["id"] + ".png")
             msg.channel.send("<" + username + ">", {files: ["https://crafatar.com/renders/head/" + data["id"] + ".png"]});
         }
+    },
+    ".body" : async (msg, [username])=>{
+        const response = await fetch('https://api.mojang.com/users/profiles/minecraft/' + username);
+        const data = await response.json();
+        if(data != null){
+            console.log("https://crafatar.com/renders/body/" + data["id"] + ".png")
+            msg.channel.send("<" + username + ">", {files: ["https://crafatar.com/renders/body/" + data["id"] + ".png"]});
+        }
     }
 }
