f00b8ee7e554ddc7edda3330e63f6716b857362f
shikto-48 <90758703+shikto-48@users.noreply.github.com>
2021-09-27 14:33:33 +0600

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,6 +1,16 @@
-exports.COMMANDS = {
+import fetch from 'node-fetch';
+
+export let COMMANDS = {
     ".ip" : "hypocraft.apexmc.co",
     ".mention" : (msg, [user])=>{
         return user
+    },
+    ".head" : async (msg, [username])=>{
+        const response = await fetch('https://api.mojang.com/users/profiles/minecraft/' + username);
+        const data = await response.json();
+        if(data != null){
+            console.log("https://crafatar.com/renders/head/" + data["id"] + ".png")
+            msg.channel.send("<" + username + ">", {files: ["https://crafatar.com/renders/head/" + data["id"] + ".png"]});
+        }
     }
 }
